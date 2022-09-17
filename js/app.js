724e1e5990e4e3c43bb08410568cf9aaa1d7d55e
Daniil <uskov.danya@gmail.com>
2022-09-17 19:20:46 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const sliderMain = new Swiper('.slider_main', {
+  freeMode: true,
+  centeredSlides: true,
+  mousewheel: true,
+  parallax: true,
+  slidesPerView: 3.5,
+  breakpoints: {
+    0: {
+      slidesPerView: 2.5,
+      spaceBetween: 20
+    },
+    680: {
+      slidesPerView: 3.5,
+      spaceBetween: 60
+
+    }
+  }
+
+
+});
