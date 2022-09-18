c5699540cc345e176bad1f080ef8de6f501d39ac
Daniil <uskov.danya@gmail.com>
2022-09-18 07:52:59 +0300

    finish

--- js/app.js
+++ js/app.js
@@ -15,6 +15,24 @@ const sliderMain = new Swiper('.slider_main', {
 
     }
   }
+});
 
+const sliderBg = new Swiper('.slider_bg', {
+  centeredSlides: true,
+  parallax: true,
+  spaceBetween: 60,
+  slidesPerView: 3.5
+});
 
+sliderMain.controller.control = sliderBg;
+
+document.querySelectorAll('.slider__item').forEach(item => {
+  item.addEventListener('click', evet => {
+    item.classList.toggle('opened');
+  });
+});
+
+let desc = document.querySelector('.description');
+sliderMain.on('slideChange', e => {
+  sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden');
 });
